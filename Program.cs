90742cf2ceda607dcfe9dcae3de82af5245f2510
isaacrojo <60981406+isaacrojo@users.noreply.github.com>
2020-02-23 21:12:31 -0700

    Primera práctica Hello World - actualizado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proyecto_Visual_Studio_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
